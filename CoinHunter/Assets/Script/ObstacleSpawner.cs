5c31b9a2669afb6f2015f818f7e68320807bfa09
김의중 <htp31698@naver.com>
2025-08-18 17:38:03 +0900

    FallingObject

--- CoinHunter/Assets/Script/ObstacleSpawner.cs
+++ CoinHunter/Assets/Script/ObstacleSpawner.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject fallPrefap;
+
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 1.5f;
+    public Vector3 spawnPoint;
+
+
+    private float interval;
+    private float timer;
+
+    private void OnEnable()
     {
-        
+        timer = 0;
+        interval = Random.Range(spawnRateMin, spawnRateMax);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer > interval)
+        {
+            spawnPoint = new Vector3(Random.Range(-8f, 8f), 10f, Random.Range(-8f, 8f));
+            GameObject go = Instantiate(fallPrefap, spawnPoint, fallPrefap.transform.rotation);
+            timer = 0;
+            interval = Random.Range(spawnRateMin, spawnRateMax);
+        }
     }
 }
